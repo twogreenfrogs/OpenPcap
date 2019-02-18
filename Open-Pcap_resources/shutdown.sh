a1c5f3ba213caf211600a94978c6eba98dc97206
twogreenfrogs <inzoolee@hotmail.com>
2019-02-18 13:50:23 -0500

    add 32bit OpenPcap binary

--- Open-Pcap_resources/shutdown.sh
+++ Open-Pcap_resources/shutdown.sh
@@ -2,5 +2,5 @@
 
 # turn off wifi when shutdown down
 echo default-on > /sys/class/leds/gl-usb150\:green\:wlan/trigger 2> /dev/null
-echo 0 > /sys/class/leds/gl-usb150\:green\:wlan/brightness 2> /dev/null
+ echo 0 > /sys/class/leds/gl-usb150\:wlan/brightness 2> /dev/null
 poweroff
